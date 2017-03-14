8a03020ef5ed017bdefd0fb11f963d4fcc829cb4
Carlos Giron <cgironalas@gmail.com>
2017-03-13 18:53:53 -0600

    Intento de sacar cruces

--- test.c
+++ test.c
@@ -4,9 +4,120 @@
 #include <string.h>
 #include <math.h>
 
-int main(int argc, char *arcgv[]){
-	int n = 5;
+int n = 2;
+int alleles;
+int offspringAmount;
 
+//progenitor1 [Aa]
+//progenitor2 [Cc]
+	//		A 		a
+	//C 	AC 		aC
+	//c 	Ac 		ac
+
+//progenitor1 [AaBb]
+//progenitor2 [CcDd]
+	// 		AB 		Ab 		aB 		ab
+	//CD 	ABCD 	AbCD 	aBCD	abCD
+	//Cd 	ABCd 	AbCd 	aBCd 	abCd
+	//cD 	ABcD 	AbcD 	aBcD 	abcD
+	//cd 	ABcd 	Abcd 	aBcd 	abcd
+void cross(char* progenitor1, char* progenitor2){
+	int tableSide = pow(2, n);
+	int value = tableSide / 2;
+
+	char row[tableSide][n];
+	char col[tableSide][n];
+	int counter, iteration, counter1, letter;
+
+	for(letter = 0; letter < n; letter++){
+		iteration = 0;
+		counter1 = 0;
+		while(iteration < tableSide){
+			counter = 0;
+			while(counter < value){
+				row[iteration][letter] = progenitor1[(letter * 2) + counter1];
+				col[iteration][letter] = progenitor2[(letter * 2) + counter1];
+				iteration++;
+				counter++;
+			}
+			counter1++;
+			if(counter1 == n){
+				counter1 = 0;
+			}
+		}
+		value /= 2;	
+	}
+
+
+	printf("1: %s\n", progenitor1);
+	printf("2: %s\n", progenitor2);
+	printf("Cross:\n");
+	for(counter = 0; counter < tableSide; counter++){
+		printf("row: ");
+		printf("%c", row[counter][0]);
+		printf("%c\n", row[counter][1]);
+	}
+	for(counter = 0; counter < tableSide; counter++){
+		printf("column: ");
+		printf("%c", col[counter][0]);
+		printf("%c\n", col[counter][1]);
+	}
+
+	char offspring[tableSide][tableSide][2 * n];
+	for(counter = 0; counter < tableSide; counter++){
+		for(counter1 = 0; counter1 <tableSide; counter1++){
+			for(letter = 0; letter < n; letter++){
+				offspring[counter][counter1][letter] = '1';
+				offspring[counter][counter1][n + letter] ='1'; 
+			}
+		}
+	}
+	/*
+	alleles = 2 * n;
+	offspringAmount = pow(4, n);
+
+	int letter = 0;
+	int currentOffspring, allele, prog1, prog2;
+	char offspring[offspringAmount][alleles];
+	
+	int tableSide = pow(2, n);
+
+	//for(letter = 0; letter < n; letter++){
+		currentOffspring = 0;
+		while(currentOffspring < offspringAmount){
+			for(allele = 0; allele < n; allele++){
+				//value = pow(2, n - allele);
+				printf("%i\n", value);
+				printf("%i\n", currentOffspring / tableSide);
+				prog1 = (2 * allele) + currentOffspring % 2;
+				prog2 = (2 * allele) + currentOffspring / tableSide;
+
+				offspring[currentOffspring][allele] = progenitor1[prog1];
+				offspring[currentOffspring][n + allele] = progenitor2[prog2];
+
+				printf("1-%i: %i - %c   ", allele, prog1, progenitor1[prog1]);
+				printf("2-%i: %i - %c   ;", allele, prog2, progenitor2[prog2]);
+			}
+			putchar('\n');
+			currentOffspring++;
+		}*/
+	//}
+	char expected[16][4] = {"ABCD", "AbCD", "aBCD", "abCD", "ABCd", "AbCd", "aBCd", "abCd", "ABcD", "AbcD", "aBcD","abcD", "ABcd", "Abcd", "aBcd", "abcd"};
+	/*
+	for(currentOffspring = 0; currentOffspring < offspringAmount; currentOffspring++){
+		printf("%c", expected[currentOffspring][0]);
+		printf("%c - ", expected[currentOffspring][1]);
+		//printf("%c", expected[currentOffspring][2]);
+		//printf("%c - ", expected[currentOffspring][3]);
+
+		printf("%c", offspring[currentOffspring][0]);
+		printf("%c\n", offspring[currentOffspring][1]);
+		//printf("%c", offspring[currentOffspring][2]);
+		//printf("%c\n", offspring[currentOffspring][3]); 
+	}*/
+}
+
+int main(int argc, char *arcgv[]){
 	int letter = 0;
 	char caps[5] = {'A','B','C','D','E'};
 
@@ -17,7 +128,6 @@ int main(int argc, char *arcgv[]){
 		genotypes[letter][0][0] = caps[letter];
 		genotypes[letter][0][1] = caps[letter];
 		
-
 		genotypes[letter][1][0] = caps[letter];
 		genotypes[letter][1][1] = tolower(caps[letter]);
 		
@@ -63,4 +173,7 @@ int main(int argc, char *arcgv[]){
 		possibleGenotypes[currentGenotype][(n*2)] = '\0';
 		printf("%s\n", possibleGenotypes[currentGenotype]);
 	}
+
+
+	cross("AaBb", "CcDd");	
 }
